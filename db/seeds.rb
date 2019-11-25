87524486ff8739e7de7ea8abcd3c7679c49cf8ea
Alberto Tarantino <albertotarantino75@yahoo.it>
2019-11-25 17:22:42 +0100

    created seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
+puts "Destroy previous Kitas"
+Kita.destroy_all
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Kitas = [{name: "Sunshine Kita", address: "Leopoldstraße 50, Berlin", language: "English-German", availability: "Full", capacity: 200},
+          {name: "Meine Erste Kita", address: "Brandenburgische Straße 22, Berlin", language: "English-German", availability: "Available", capacity: 84},
+          {name: "Charlotte", address: "Genslerstraße 21, Berlin", language: "German", availability: "Avalilable", capacity: 120},
+          {name: "Tutti Amici", address: "Leopoldßtraße 82, Berlin", language: "Italian-German", availability: "Available", capacity: 64},
+          {name: "Le Tour Eiffel", address: "Holteistraße 18, Berlin", language: "French-German", availability: "Full", capacity: 35},
+          {name: "Detsky Sad Kita", address: "Weisbachstraße 1-4, Berlin", language: "Russian-German", availability: "Available", capacity: 65},
+          {name: "Stesti Kita", address: "Schulweg, Berlin", language: "Czech-German", availability: "Full", capacity: 25},
+          {name: "Boldog Gyerekek Kita", address: "Lichtenberger Straße, Berlin", language: "Hungarian-German", availability: "Available", capacity: 46},
+          {name: "Happikizzu", address: "Jacobystraße 5-6, Berlin", language: "Japanese-German", availability: "Full", capacity: 38},
+          {name: "Szczescie", address: "Bornholmer Straße 2, Berlin", language: "Polish-German", availability: "Available", capacity: 52}]
