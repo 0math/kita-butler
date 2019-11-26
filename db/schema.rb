2277ff634d756c2457751de0999ecd178a878df3
Alberto Tarantino <albertotarantino75@yahoo.it>
2019-11-26 11:42:57 +0100

    create application policy

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
 ActiveRecord::Schema.define(version: 2019_11_25_172324) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +56,6 @@ ActiveRecord::Schema.define(version: 2019_11_25_172324) do
     t.index ["reservation_id"], name: "index_reviews_on_reservation_id"
   end
 
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -73,10 +69,8 @@ ActiveRecord::Schema.define(version: 2019_11_25_172324) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
   add_foreign_key "kids", "users"
   add_foreign_key "reservations", "kids"
   add_foreign_key "reservations", "kindergardens"
   add_foreign_key "reviews", "reservations"
-
 end
