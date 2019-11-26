b94b3ecf295d4543e1d7428c6db46a0877063c18
Andrei Tigai <andrey.tigay@gmail.com>
2019-11-26 14:53:06 +0100

    remove link to favourites

--- app/models/kindergarden.rb
+++ app/models/kindergarden.rb
@@ -1,6 +1,6 @@
 class Kindergarden < ApplicationRecord
   has_many :reservations
-  belongs_to :favourite
+  # belongs_to :favourite
   has_many :reviews, through: :reservations
   # we don't validate kindergardens for now, as they are hardcoded by us
 end
