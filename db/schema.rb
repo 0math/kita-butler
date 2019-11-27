f6bb04414ae693b8fbaf2c66bbccaa4019236921
Elena-Learning <elena.urban.professionell@gmail.com>
2019-11-27 10:43:01 +0100

    added coordinates through migration to kindergardens

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_110328) do
+ActiveRecord::Schema.define(version: 2019_11_27_094150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2019_11_26_110328) do
     t.datetime "updated_at", null: false
     t.string "phone"
     t.string "email"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "reservations", force: :cascade do |t|
