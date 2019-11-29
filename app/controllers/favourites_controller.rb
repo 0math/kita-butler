fe8418f40e81d013c78cfc2efe63176594b23239
Andrei Tigai <andrey.tigay@gmail.com>
2019-11-29 13:10:31 +0100

    remove extra comments

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -3,13 +3,10 @@ class FavouritesController < ApplicationController
   def create
     # @user = current_user.id -- if we chose to link favourites and users tables
     @kid = current_user.kids.first
-    # raise
-    @kindergarden = Kindergarden.find(params[:kindergarden_id]) # fix find
+    @kindergarden = Kindergarden.find(params[:kindergarden_id])
     @favourite = Favourite.new(kid: @kid, kindergarden: @kindergarden)
     authorize @favourite
-    # raise
     @favourite.save
-    # remove redirect_to later
     redirect_to dashboard_path
   end
 
