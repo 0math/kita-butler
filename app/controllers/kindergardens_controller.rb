e56a130d3620f430ff48a28a7aafdd7bc62f52a7
Alberto Tarantino <albertotarantino75@yahoo.it>
2019-12-03 16:25:17 +0100

    fixed advanced search

--- app/controllers/kindergardens_controller.rb
+++ app/controllers/kindergardens_controller.rb
@@ -5,100 +5,26 @@ class KindergardensController < ApplicationController
   def index
     @kindergardens = policy_scope(Kindergarden).geocoded
     if params[:query].present?
-      return @kindergardens = Kindergarden.search_by_name_and_address_and_language(params[:query])
+      @kindergardens = @kindergardens.search_by_name_and_address_and_language(params[:query])
+      if @kindergardens.empty?
+        flash[:alert] = "Sorry no KiTa matches your search."
+        redirect_to(root_path)
+      else
+        @kindergardens = filter_results(@kindergardens)
+        @markers = show_markers(@kindergardens)
+          if @kindergardens.empty?
+            flash[:alert] = "Sorry no KiTa matches your search."
+            redirect_to(root_path)
+          end
+      end
+    elsif params[:query].blank?
+      @kindergardens = filter_results(@kindergardens)
+      @markers = show_markers(@kindergardens)
+    else
+      flash[:alert] = "Sorry no KiTa matches your search."
+      redirect_to(root_path)
     end
 
-    @kindergardens = Kindergarden.search_by_name_and_address_and_language(params[:search])
-
-    if params[:opening_hours]
-      @kindergardens = @kindergardens.advanced_search(params[:opening_hours])
-    end
-
-    if params[:closing_hours]
-      @kindergardens = @kindergardens.advanced_search(params[:closing_hours])
-    end
-
-    if params[:size]
-      @kindergardens = @kindergardens.advanced_search(params[:size])
-    end
-
-    if params[:age_structure]
-      @kindergardens = @kindergardens.advanced_search(params[:age_structure])
-    end
-
-    if params[:edu_system]
-      @kindergardens = @kindergardens.advanced_search(params[:edu_system])
-    end
-
-    if params[:aesthetic_edu]
-      @kindergardens = @kindergardens.advanced_search(params[:aesthetic_edu])
-    end
-
-
-
- #    {"search"=>"Berlin",
- # "opening_hours"=>"... Opening hours...",
- # "closing_hours"=>"... Closing hours...",
- # "size"=>"... Size...",
- # "age_structure"=>"Mixed ages",
- # "edu_system"=>"... Educational System...",
- # "aesthetic_edu"=>"... Aesthetic Education..."}
-
-
-
-    # @kindergardens = @kindergardens.advanced_search(params[:age_structure]).advanced_search(params[:size])
-
-      # if params[:opening_hours] || params[:closing_hours] || params[:size] || params[:age_structure] || params[:edu_system] || params[:aesthetic_edu]
-
-
-        # sql_query2 = "kindergardens.opening_hours @@ :opening_hours\
-        #                 kindergardens.closing_hours @@ :closing_hours\
-        #                 kindergardens.size @@ :size\
-        #                 kindergardens.age_structure @@ :age_structure\
-        #                 kindergardens.edu_system @@ :edu_system\
-        #                kindergardens.aesthetic_edu @@ :aesthetic_edu"
-
-        # @kindergardens = @kindergardens.where(sql_query2, opening_hours: "%#{params[:opening_hours]}%",
-        #                                                    closing_hours: "%#{params[:closing_hours]}%",
-        #                                                      size: "%#{params[:size]}%",
-        #                                                      age_structure: "%#{params[:age_structure]}%",
-        #                                                      edu_system: "%#{params[:edu_system]}%",
-        #                                                      aesthetic_edu: "%#{params[:aesthetic_edu]}%")
-
-
-
-
-      # if @kindergardens.exists?
-      #  return @kindergardens
-
-
-
-      #   if (params[:opening_hours] || params[:closing_hours] || params[:size] || params[:age_structure] || params[:edu_system] || params[:aesthetic_edu]).present?
-
-      #     sql_query2 = "kindergardens.opening_hours @@ :opening_hours\
-      #                   kindergardens.closing_hours @@ :closing_hours\
-      #                   kindergardens.size @@ :size\
-      #                   kindergardens.age_structure @@ :age_structure\
-      #                   kindergardens.edu_system @@ :edu_system\
-      #                   kindergardens.aesthetic_edu @@ :aesthetic_edu"
-
-      #     @kindergardens = @kindergardens.where(sql_query2, opening_hours: "%#{params[:opening_hours]}%",
-      #                                                       closing_hours: "%#{params[:closing_hours]}%",
-      #                                                       size: "%#{params[:size]}%",
-      #                                                       age_structure: "%#{params[:age_structure]}%",
-      #                                                       edu_system: "%#{params[:edu_system]}%",
-      #                                                       aesthetic_edu: "%#{params[:aesthetic_edu]}%")
-
-      #   end
-      # else
-      #   redirect_to root_path(message: "Sorry no KiTa matches your search")
-      # end
-     @markers = @kindergardens.map do |kindergarden|
-      {
-        lat: kindergarden.latitude,
-        lng: kindergarden.longitude
-      }
-    end
   end
 
   def show
@@ -112,7 +38,44 @@ class KindergardensController < ApplicationController
     @markers = [{ lat: @kindergarden.latitude, lng: @kindergarden.longitude }]
   end
 
+  def filter_results(kindergardens)
+    if params[:opening_hours].present?
+      @kindergardens = kindergardens.where(opening_hours: params[:opening_hours])
+    end
+
+    if params[:closing_hours].present?
+      @kindergardens = kindergardens.where(closing_hours: params[:closing_hours])
+    end
+
+    if params[:size].present?
+      @kindergardens = kindergardens.where(size: params[:size])
+    end
+
+    if params[:age_structure].present?
+      @kindergardens = kindergardens.where(age_structure: params[:age_structure])
+    end
+
+    if params[:edu_system].present?
+      @kindergardens = kindergardens.where(edu_system: params[:edu_system])
+    end
+
+    if params[:aesthetic_edu].present?
+      @kindergardens = kindergardens.where(aesthetic_edu: params[:aesthetic_edu])
+    end
+    @kindergardens
+  end
+
+
   private
+
+  def show_markers(kindergardens)
+    @markers = kindergardens.map do |kindergarden|
+      {
+        lat: kindergarden.latitude,
+        lng: kindergarden.longitude
+      }
+    end
+  end
 
   def kindergarden_params
     params.require(:kindergarden).permit(:name, :address, :language, :capacity, :photo, :email, :phone, :is_available,
