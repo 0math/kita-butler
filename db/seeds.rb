597e011ff26bc34e9a205a7e3f7f2e148cc740cf
Andrei Tigai <andrey.tigay@gmail.com>
2019-12-05 15:41:41 +0100

    fix seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 puts "Destroy previous Kitas"
+Reservation.destroy_all
 Kindergarden.destroy_all
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
